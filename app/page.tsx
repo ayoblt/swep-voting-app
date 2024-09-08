7bdbf5230748f143a60fe1b71ee6bd2023fcc3e6
Lateef Taiwo <ayomidepakoo@gmail.com>
2024-09-08 10:22:55 +0100

    change home page style

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <section className="flex min-h-screen flex-col items-center justify-center p-24 space-y-5">
+    <section className="flex min-h-screen flex-col items-center text-center justify-center p-24 space-y-5">
       <h1 className="font-bold text-xl">SWEP 200 Student Voting App</h1>
       <Link
         href="/admin/accounts/login"
