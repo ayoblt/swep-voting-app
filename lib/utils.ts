077d5cfe71c6f63ba788643f96ce3443ebb3170a
Lateef Taiwo <ayomidepakoo@gmail.com>
2024-09-09 12:16:29 +0100

    minor change to formatTime

--- lib/utils.ts
+++ lib/utils.ts
@@ -70,7 +70,6 @@ export function formatTime(timeString: string) {
     const [hours, minutes] = timeString.split(':').map(Number);
     const date = new Date();
     date.setHours(hours, minutes);
-    date.setHours(date.getHours() + 1)
     return date.toLocaleTimeString('en-US', {
       hour: '2-digit',
       minute: '2-digit',
@@ -78,6 +77,7 @@ export function formatTime(timeString: string) {
     });
   } else {
     const date = new Date(timeString);
+    date.setHours(date.getHours() + 1)
     return date.toLocaleTimeString('en-US', {
       hour: 'numeric',
       minute: '2-digit',
